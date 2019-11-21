3919bb677df3b7ded8cdbbe9b60f76f7742ffc4d
TinTin-DXQ <734752757@qq.com>
2019-11-21 17:12:00 +0800

    autotest

--- generate.py
+++ generate.py
@@ -55,34 +55,72 @@ def SSIM(img1 ,img2):
 
 
 def one_attack(img):
-	count=0
-	rImg=copy.copy(img)
-	while count<=784:
-		x=random.randint(0,27)
-		y=random.randint(0,27)
-		rImg[x][y]=random.random()
-		before=np.argmax(model.predict(np.expand_dims(img,0)))
-		after=np.argmax(model.predict(np.expand_dims(rImg,0)))
-		if before==after:
-			count=count+1;
-		else:
-			score=SSIM(rImg,img)
-			if score<0.95:
-				continue
-			else:
-				print("one!!!")
-				return rImg
-	return img
+    count=0
+    while count<=784:
+        rImg=copy.copy(img)
+        for n in range(5):
+            x=random.randint(0,27)
+            y=random.randint(0,27)
+            rImg[x][y]=random.random()
+        before=np.argmax(model.predict(np.expand_dims(img,0)))
+        after=np.argmax(model.predict(np.expand_dims(rImg,0)))
+        if before==after:
+            count=count + 1
+        else:
+            print("one!!!")
+            return rImg
+    return img
+
+def two_attack(img):
+    count = 0
+    while count <= 30:
+        rImg = copy.copy(img)
+        for n in range(10):
+            x1 = random.randint(0, 27)
+            y1 = random.randint(0, 27)
+            x2=random.randint(0,27)
+            y2=random.randint(0,27)
+            temp=rImg[x1][y1]
+            rImg[x1][y1] = rImg[x2][y2]
+            rImg[x2][y2]=temp
+        before = np.argmax(model.predict(np.expand_dims(img, 0)))
+        after = np.argmax(model.predict(np.expand_dims(rImg, 0)))
+        if before == after:
+            count = count + 1
+        else:
+            print("two!!!")
+            return rImg
+    return img
+
+def last_attack(img):
+    rImg=copy.copy(img)
+    for i in range(25):
+        for j in range(25):
+            rImg[i][j]=random.random()
+    print("three")
+    return rImg
 
 def generate(images,shape):
     print("fdfsdf")
+
     generate_images=np.empty_like(images)
     test_images=images/255.0
+    count=0
     for test_image in test_images:
+        print(count)
+        if(count>10):
+            break
         cImg=one_attack(test_image)
         before=np.argmax(model.predict(np.expand_dims(test_image, 0)))
         after=np.argmax(model.predict(np.expand_dims(cImg, 0)))
-        generate_images = np.append(generate_images, cImg)
+        if(before==after):
+            cImg=two_attack(test_image)
+            before = np.argmax(model.predict(np.expand_dims(test_image, 0)))
+            after = np.argmax(model.predict(np.expand_dims(cImg, 0)))
+            if(before==after):
+                cImg = last_attack(test_image)
+        generate_images[count] = cImg
+        count=count+1
     return generate_images
 
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.fashion_mnist.load_data()
